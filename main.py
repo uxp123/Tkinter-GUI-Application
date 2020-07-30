d539b0a30c38210e81c2bdca698d0feab2ff7127
uxp123 <57874831+uxp123@users.noreply.github.com>
2020-07-30 21:32:41 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import font
+import requests
+
+HEIGHT = 700
+WIDTH = 800
+
+
+def test_function(entry):
+    print('This is the entry:', entry) 
+
+
+def format_response(weather):
+   print(weather)
+   try:
+    name = weather['name']
+    desc = weather['weather'][0]['description']
+    temp = weather['main']['temp']
+
+    final_str = 'City: %s \nConditions: %s \nTemperature (°C): %s' % (name, desc, temp)
+   except:
+    final_str = 'There was a problem retrieving that information'
+
+   return final_str
+
+
+
+def get_weather(city):
+    weather_key = 'a4d3b333142cc08417d242ad953a9fd3'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': weather_key, 'q': city, 'units': 'metric'}
+    response = requests.get(url, params=params)
+    weather = response.json()
+
+    label['text'] = format_response(weather)
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=HEIGHT)
+canvas.pack()
+
+background_image = tk.PhotoImage(file='landscape.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='#00e0ff', bd=5)  # blue
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry = tk.Entry(frame, font=40)  # green
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text='Get Weather', font=('Krungthep, 18'), cursor=('star'), command=lambda: get_weather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(root, bg='#00e0ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, font=('AppleGothic, 18'), anchor='nw', justify='left', bd=4) # yellow
+label.place(relwidth=1, relheight=1)
+
+root.title("Papsy Weather App")
+
+root.mainloop()
+#============================
+# API KEY = a4d3b333142cc08417d242ad953a9fd3
+# API URL{PRIVATE ACCOUNT} = api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
